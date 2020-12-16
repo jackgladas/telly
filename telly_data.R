a77e3dcfb963b840209b5dcb59a03f218d4401c1
jackgladas <jackgladas@gmail.com>
2020-12-16 21:55:46 +0000

    importing imdb data

--- telly_data.R
+++ telly_data.R
@@ -2,24 +2,60 @@
 library(googlesheets4)
 library(tidyverse)
 
-#import raw data ----
-import_films <- read_sheet("1LRJZX2ErRrWdxo4VAyyVbrUuEy4SZ1zUgr80aksLra0", sheet = "Films")
-import_series <- read_sheet("1LRJZX2ErRrWdxo4VAyyVbrUuEy4SZ1zUgr80aksLra0", sheet = "Series")
+#import raw personal data ----
+import_personal_films <- read_sheet("1LRJZX2ErRrWdxo4VAyyVbrUuEy4SZ1zUgr80aksLra0", sheet = "Films")
+import_personal_series <- read_sheet("1LRJZX2ErRrWdxo4VAyyVbrUuEy4SZ1zUgr80aksLra0", sheet = "Series")
 
+#import raw IMDb data ----
+download.file("https://datasets.imdbws.com/title.basics.tsv.gz",
+              destfile = "title.basics")
+import_imdb_titles <- read_delim("title.basics",
+                                  delim = "\t",
+                                  col_types = cols(
+                                    tconst = "c",
+                                    titleType = "c",
+                                    primaryTitle = "c",
+                                    originalTitle = "c",
+                                    isAdult = "f",
+                                    startYear = 'i',
+                                    endYear = 'i',
+                                    runtimeMinutes = 'i',
+                                    genres = 'c'
+                                  ),
+                                  na = c("",
+                                         "NA",
+                                         "\\N",
+                                         " "),
+                                  quoted_na = FALSE,
+                                  escape_double = FALSE)
+download.file("https://datasets.imdbws.com/title.episode.tsv.gz",
+              destfile = "title.episode")
+import_imdb_episodes <-read_tsv("title.episode")
 
-
-#basic data cleaning ----
-clean_films <- import_films %>%
+#basic data cleaning (no joins yet) ----
+clean_films <- import_personal_films %>%
   mutate(watched_film_id = row_number()) %>%
   rename(watched_date = Date,
          film_name = Name,
          rating = Rating,
-         cinema = Cinema) %>%
-  mutate(cinema_flag = as.factor(cinema)) %>%
+         cinema = Cinema,
+         first_watch = 'First Watch',
+         imdb_key = IMDb) %>%
+  mutate(cinema_flag = as.factor(cinema),
+         first_watch_flag = as.factor(first_watch)) %>%
   select(watched_film_id,
          watched_date,
          film_name,
          rating,
-         cinema_flag)
+         cinema_flag,
+         first_watch_flag,
+         imdb_key)
 
-## import IMDB data from: https://www.imdb.com/interfaces/
+x_checks <- clean_films %>%
+  left_join(import_imdb_titles, c("imdb_key" = "tconst")) %>%
+  select(film_name, primaryTitle)
+
+films <- clean_films %>%
+  left_join(import_imdb_titles, c("imdb_key" = "tconst"))
+#remove raw imports
+rm(import_personal_films)
